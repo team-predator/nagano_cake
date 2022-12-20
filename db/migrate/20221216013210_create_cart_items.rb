bc4130601865ec478834b947e51802a95a263c2a
MasashiIde <117148113+MasashiIde@users.noreply.github.com>
2022-12-20 06:43:56 +0000

    add public-items-show

--- db/migrate/20221216013210_create_cart_items.rb
+++ db/migrate/20221216013210_create_cart_items.rb
@@ -1,6 +1,8 @@
 class CreateCartItems < ActiveRecord::Migration[6.1]
   def change
     create_table :cart_items do |t|
+      t.integer :customer_id, null: false
+      t.integer :item_id, null: false
       t.integer :amount, null: false
       t.timestamps
     end
