9ab674a6cda7ed2e54030e4ef3fc5f11c795d37e
EC2 Default User <ec2-user@ip-172-31-36-94.ap-northeast-1.compute.internal>
2022-12-20 06:32:59 +0000

    Finish

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -9,6 +9,9 @@ class Public::CustomersController < ApplicationController
   end
   
   def update
+    @customer = current_customer
+    @customer.update(customer_params)
+    redirect_to customers_my_page_path(current_customer)
   end
 
   def unsubscribe
@@ -24,7 +27,7 @@ class Public::CustomersController < ApplicationController
   private
 
   def customer_params
-    params.require(:cuctomer).permit(:last_mane, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :email, :is_deleted)
   end
 
 end
