a24357113c26bf7cf41244eda3177b66a6ca7806
MasashiIde <117148113+MasashiIde@users.noreply.github.com>
2022-12-23 06:37:47 +0000

    add admin_orders-screen

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,19 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: params[:id])
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: params[:id])
+    @order.update(order_params)
+    render 'show'
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
   end
 end
