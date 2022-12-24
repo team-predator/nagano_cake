d7e918786d0896236c23f18de59f0e48f5fb8877
MasashiIde <117148113+MasashiIde@users.noreply.github.com>
2022-12-24 06:54:10 +0000

    add

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,8 +7,10 @@ class Admin::OrdersController < ApplicationController
   def update
     @order = Order.find(params[:id])
     @order_details = OrderDetail.where(order_id: params[:id])
-    @order.update(order_params)
-    render 'show'
+    if @order.update(order_params)
+      @order_details.update_all(making_status:1) if @order.status == "confirm_payment"
+    end
+    redirect_to admin_order_path(@order)
   end
 
   private
