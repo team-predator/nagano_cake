c38f74873849a757aee108a4943864a9d0c9d469
EC2 Default User <ec2-user@ip-172-31-36-94.ap-northeast-1.compute.internal>
2022-12-24 06:30:23 +0000

    不具合修正

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,10 +1,10 @@
 class Public::AddressesController < ApplicationController
-  
+
   before_action :authenticate_customer!
 
   def index
     @address = Address.new
-    @addresses = Address.all
+    @addresses = current_customer.addresses
   end
 
   def create
